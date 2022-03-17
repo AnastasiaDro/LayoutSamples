bb1714d5c0c5eacfe45c10174f16669fa35c58c3
Anastasia Drogunova <adrogunova@gmail.com>
2022-03-17 03:24:31 +0300

    добавила relativeActivity

---
+++ app/src/main/java/com/example/myapplication/RelativeActivity.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.widget.SwitchCompat
+import androidx.constraintlayout.widget.Group
+
+class RelativeActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_relative)
+    }
+
+
+}
